4c81ac05f3d0645f1247cc584387028b4e7a71c9
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-02-16 10:57:49 +0900

    added desc for exercise 6

---
+++ missing_number_game.rb
@@ -0,0 +1,5 @@
+# A Fibonacci Sequence is created by adding two numbers to create the next number in the sequence.
+# You then sum that number with the one preceding it to get the next number,, and so on.
+# For example, if you have the sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, then the next number in the sequence is 55.
+# To do this in Ruby, you can use a “while” expression to create a starting value and then
+# tell Ruby which integers to add together.
