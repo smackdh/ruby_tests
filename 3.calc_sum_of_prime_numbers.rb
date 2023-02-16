c8990a2a5ef59c63c7e27e428a0bb0cf50b7af93
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-02-16 10:27:15 +0900

    add method takes arr

--- 3.calc_sum_of_prime_numbers.rb
+++ 3.calc_sum_of_prime_numbers.rb
@@ -1,16 +1,18 @@
 # When calculating the sum of prime numbers, you first have to identify the prime numbers within an array.
 # You then have to add all of these prime numbers together. In Ruby, you can call an expression that selects
 # the prime numbers from an array and then use the “reduce” expression to add them together.
-def find_prime_number(num)
-  if num <= 1 || num.even?
-    prime = false
-  elsif (2..Math.sqrt(num)).all? {|i| (num % i ).nonzero? }
-    prime = true
-  else
-    prime = false
-  end
+def find_prime_number(arr)
+  arr.each do |number|
+    if number <= 1 || number.even?
+      prime = false
+    elsif (2..Math.sqrt(number)).all? {|i| (number % i ).nonzero? }
+      prime = true
+    else
+      prime = false
+    end
+    puts prime
 
-  puts prime
+end
 end
 
-find_prime_number(3)
+find_prime_number([3,0])
