d8058f19e3244455b108d1eed0e671574b947ac3
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-02-16 13:40:01 +0900

    solved fibonnaci

--- 6.fibonacci_algorithm.rb
+++ 6.fibonacci_algorithm.rb
@@ -3,3 +3,22 @@
 # For example, if you have the sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, then the next number in the sequence is 55.
 # To do this in Ruby, you can use a “while” expression to create a starting value and then
 # tell Ruby which integers to add together.
+
+# -- Psuedo --- #
+# 1. Method takes one number.
+# 2. Then as long as max is less than 100
+# 3. Number = []
+# 4. max = number.last + number[-1]
+def fibonnaci_count(max)
+  count = 0
+  arr = [0,1]
+  while count <= max
+    count = arr.last + arr[-2]
+    arr << count
+  end
+  arr
+end
+
+
+sequence = fibonnaci_count(2000)
+puts "The sequence is: #{sequence}"
