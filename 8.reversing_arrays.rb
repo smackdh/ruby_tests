4458439d63fa83ae67de60a83bf91dcf6e3f3a8c
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-02-16 14:14:04 +0900

    finihed reverse array

---
+++ 8.reversing_arrays.rb
@@ -0,0 +1,11 @@
+# In this problem, your objective is to reverse the order of the numbers in an array.
+# For example, if your array was [8, 2, 9, 4], you would want your code to return [4, 9, 2, 8].
+# To accomplish this in Ruby, your primary tool will be the “reverse” expression,
+# which examines an array and flips the order of the values in it. You’ll also use a “while”
+# expression and designate the position of each number to dictate which numbers to apply the “reverse” expression to.
+
+# -- PSUEDO -- #
+# 1. This challenge was likely outdated, as you can use reverse directly on an array either way.
+
+array = [3,2,1]
+puts array.reverse
